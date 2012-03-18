f5c29e5dbf97731c074b18a8aaa2677278bb9642
RebeccaLea.Rush@gmail.com <RebeccaLea.Rush@gmail.com@098c6ad9-869e-9a4e-4e48-689b8fa337fd>
2012-03-18 18:58:27 +0000

    still working

---  cs-430-app/MysteryApp/src/com/android/mysteryApp/MysteryAppActivity.java
+++  cs-430-app/MysteryApp/src/com/android/mysteryApp/MysteryAppActivity.java
@@ -2,6 +2,7 @@ package com.android.mysteryApp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
