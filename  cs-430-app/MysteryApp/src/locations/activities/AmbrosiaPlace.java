760df74702b51a4389d1e9a863c6d68d43798f3f
JordanHitrik@gmail.com <JordanHitrik@gmail.com@098c6ad9-869e-9a4e-4e48-689b8fa337fd>
2012-04-15 22:45:10 +0000

    no real change - just removed an import statement

---  cs-430-app/MysteryApp/src/locations/activities/AmbrosiaPlace.java
+++  cs-430-app/MysteryApp/src/locations/activities/AmbrosiaPlace.java
@@ -1,9 +1,6 @@
 package locations.activities;
 
 import com.android.mysteryApp.R;
-import com.android.mysteryApp.R.drawable;
-import com.android.mysteryApp.R.layout;
-
 import persons.activities.Ambassador;
 import persons.leaders.AmbassadorAmber;
 import android.content.Intent;
@@ -16,11 +13,6 @@ public class AmbrosiaPlace extends LocationActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ambrosia);
-//        
-//        TextView t = new TextView(this); 
-//
-//        t = (TextView)findViewById(R.id.textView1); 
-//        t.setText(Ambrosia.getAmbrosia().getName());
     }
     
     
