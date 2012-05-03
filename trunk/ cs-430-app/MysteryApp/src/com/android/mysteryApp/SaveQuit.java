11785ed9d8e0be76802573026e8cf2cef52a5dd5
JordanHitrik@gmail.com <JordanHitrik@gmail.com@098c6ad9-869e-9a4e-4e48-689b8fa337fd>
2012-05-03 05:11:37 +0000

    enhanced saving groundwork

--- trunk/ cs-430-app/MysteryApp/src/com/android/mysteryApp/SaveQuit.java
+++ trunk/ cs-430-app/MysteryApp/src/com/android/mysteryApp/SaveQuit.java
@@ -1,12 +1,17 @@
 package com.android.mysteryApp;
 
+import game.DataLoader;
+import persons.NonPlayer;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SaveQuit extends Activity {
     /** Called when the activity is first created. */
@@ -17,10 +22,27 @@ public class SaveQuit extends Activity {
     }
     
     public void saveCont(View view){
-    	SharedPreferences settings2 = getSharedPreferences("save2", MODE_PRIVATE);
+    	/*SharedPreferences settings2 = getSharedPreferences("save2", MODE_PRIVATE);
         Editor editor = settings2.edit();
         editor.putBoolean("testMode2", true);
-        editor.commit();
+        editor.commit();*/
+        
+        final DataLoader dl = DataLoader.getDataLoader();
+		final CharSequence[] saveStatuses = dl.getSaveStatuses(getApplicationContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setTitle("Select slot to save game:");
+    	builder.setItems(saveStatuses, new DialogInterface.OnClickListener() {
+    		public void onClick(DialogInterface dialog, int choice) {
+    			/*NonPlayer curr = npcs[choice];
+    			showDialog(curr.getName() + ": " + curr.getDescription(), 3000, R.drawable.president1_head);
+    			Intent characterIntent = npcs[choice].getIntent(getApplicationContext());
+    			startActivity(characterIntent);*/
+    			Toast.makeText(getApplicationContext(), saveStatuses[choice], Toast.LENGTH_LONG).show();
+    			dl.setSaveStatus(getApplicationContext(), choice, true);
+    		}
+    	});
+    	AlertDialog alert = builder.create();
+    	alert.show();
     }
     
     public void saveQuit(View view){
